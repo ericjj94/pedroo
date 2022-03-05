c4f46c8370b2995dad2b6be664f68596e4ec5a51
Eric <ericjj94@gmail.com>
2022-03-05 19:57:32 +0530

    converted locations to use MUI

--- src/pages/Locations/Locations.tsx
+++ src/pages/Locations/Locations.tsx
@@ -1,78 +1,68 @@
 import { useEffect, useState } from "react";
 import { useQuery } from "@apollo/client";
-import { Row, Col } from "reactstrap";
 import { GET_ALL_LOCATIONS } from "./query";
-import { NameStyle } from "../../styled";
 import PaginationTable from "../../components/Table/Table";
 
 const Locations = () => {
   const [locationData, setLocationData] = useState([]);
   const [page, setPage] = useState(0);
 
-  const handleRedirection = (props: any) => {};
-
-  const columns = [
-    {
-      Header: "Name",
-      accessor: "name",
-      Cell: (props: any) => {
-        return (
-          <NameStyle
-            onClick={() => {
-              handleRedirection(props);
-            }}
-          >
-            {props.value}
-          </NameStyle>
-        );
-      },
-    },
-    {
-      Header: "Type",
-      accessor: "type",
-    },
-    {
-      Header: "Dimension",
-      accessor: "dimension",
-    },
-    {
-      Header: "Action",
-    },
-  ];
-
   const { loading, error, data } = useQuery(GET_ALL_LOCATIONS, {
     variables: {
       page: page + 1,
     },
+    skip: page % 3 !== 0,
   });
 
   useEffect(() => {
     if (data?.locations?.results.length) {
-      setLocationData((prev) => [...prev, ...data.locations.results] as []);
+      setLocationData((prev) => [...prev, ...data?.locations?.results] as []);
     }
   }, [data]);
 
-  if (loading) {
+  if (loading && page === 0) {
     return <div>isloading</div>;
   }
 
-  const handlePageChange = (type: string) => {
-    if (type === "next") {
-      setPage(page + 1);
-    } else {
-      setPage(page - 1);
-    }
-  };
-
   return (
-    <Col className="container">
-      <Col sm={12}>
-        <Row>
-          <h2>Locations</h2>
-        </Row>
-      </Col>
-      <Col sm={12}>{/* <PaginationTable /> */}</Col>
-    </Col>
+    <div className="container">
+      <div className="row">
+        <h2>Locations</h2>
+      </div>
+      <div className="row">
+        <PaginationTable
+          columnData={[
+            {
+              id: "name",
+              name: "Name",
+              enableSort: true,
+              align: "center",
+            },
+            {
+              id: "type",
+              name: "Type",
+              enableSort: true,
+              align: "center",
+            },
+            {
+              id: "dimension",
+              name: "Dimension",
+              enableSort: true,
+              align: "center",
+            },
+            {
+              id: "created",
+              name: "Created",
+              enableSort: true,
+              align: "center",
+            },
+          ]}
+          rows={locationData}
+          page={page}
+          setPage={setPage}
+        />
+      </div>
+    </div>
   );
 };
 export default Locations;
